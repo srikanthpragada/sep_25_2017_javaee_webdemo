c219bffad51fa760b23ce338efb187e374ee2589
srikanthpragada <srikanthpragada@gmail.com>
2017-11-07 09:32:34 +0530

    7-NOV

--- src/rest/EmployeeService.java
+++ src/rest/EmployeeService.java
@@ -1,13 +1,20 @@
 package rest;
 
 import java.io.StringWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import javax.json.Json;
 import javax.json.stream.JsonGenerator;
 import javax.sql.rowset.CachedRowSet;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.InternalServerErrorException;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -18,11 +25,11 @@ import oracle.jdbc.rowset.OracleCachedRowSet;
 
 @Path("/employees")
 public class EmployeeService {
-	
+
 	@GET
 	@Produces("application/json")
 	public String get() {
-		
+
 		StringWriter sw = new StringWriter();
 		JsonGenerator gen = Json.createGenerator(sw);
 
@@ -38,26 +45,24 @@ public class EmployeeService {
 				gen.writeStartObject();
 				gen.write("name", crs.getString("first_name"));
 				gen.write("salary", crs.getString("salary"));
-				gen.writeEnd();  // end of object
+				gen.writeEnd(); // end of object
 			}
 
-			gen.writeEnd();  // end of array
+			gen.writeEnd(); // end of array
 			gen.close();
 			return sw.toString();
-		}
-		catch(Exception ex) {
+		} catch (Exception ex) {
 			System.out.println(ex.getMessage());
 		}
 		return null;
-		
+
 	}
-	
-	
+
 	@GET
 	@Path("/dept/{dept}")
 	@Produces("application/json")
-	public String getEmpByDept(@PathParam ("dept") String dept) {
-		
+	public String getEmpByDept(@PathParam("dept") String dept) {
+
 		StringWriter sw = new StringWriter();
 		JsonGenerator gen = Json.createGenerator(sw);
 
@@ -67,33 +72,31 @@ public class EmployeeService {
 			crs.setUsername("hr");
 			crs.setPassword("hr");
 			crs.setCommand("select * from employees where department_id = ?");
-			crs.setString(1,  dept);
+			crs.setString(1, dept);
 			crs.execute();
 
 			while (crs.next()) {
 				gen.writeStartObject();
 				gen.write("name", crs.getString("first_name"));
 				gen.write("salary", crs.getString("salary"));
-				gen.writeEnd();  // end of object
+				gen.writeEnd(); // end of object
 			}
 
-			gen.writeEnd();  // end of array
+			gen.writeEnd(); // end of array
 			gen.close();
 			return sw.toString();
-		}
-		catch(Exception ex) {
+		} catch (Exception ex) {
 			System.out.println(ex.getMessage());
 		}
-		return "[]" ; // empty array
-		
+		return "[]"; // empty array
+
 	}
 
-	
 	@GET
 	@Path("/job/{job}")
 	@Produces("application/json")
-	public String getEmpByJob(@PathParam ("job") String job) {
-		
+	public String getEmpByJob(@PathParam("job") String job) {
+
 		StringWriter sw = new StringWriter();
 		JsonGenerator gen = Json.createGenerator(sw);
 
@@ -103,34 +106,32 @@ public class EmployeeService {
 			crs.setUsername("hr");
 			crs.setPassword("hr");
 			crs.setCommand("select * from employees where job_id = ?");
-			crs.setString(1,  job);
+			crs.setString(1, job);
 			crs.execute();
 
 			while (crs.next()) {
 				gen.writeStartObject();
 				gen.write("name", crs.getString("first_name"));
 				gen.write("salary", crs.getString("salary"));
-				gen.writeEnd();  // end of object
+				gen.writeEnd(); // end of object
 			}
 
-			gen.writeEnd();  // end of array
+			gen.writeEnd(); // end of array
 			gen.close();
 			return sw.toString();
-		}
-		catch(Exception ex) {
+		} catch (Exception ex) {
 			System.out.println(ex.getMessage());
 		}
-		
-	    return "[]" ; // empty array
-		
+
+		return "[]"; // empty array
+
 	}
-	
-	
+
 	@GET
 	@Path("/jobs")
 	@Produces("application/json")
 	public String getJobs() {
-		
+
 		StringWriter sw = new StringWriter();
 		JsonGenerator gen = Json.createGenerator(sw);
 
@@ -146,28 +147,79 @@ public class EmployeeService {
 				gen.writeStartObject();
 				gen.write("id", crs.getString("job_id"));
 				gen.write("title", crs.getString("job_title"));
-				gen.writeEnd();  // end of object
+				gen.writeEnd(); // end of object
 			}
 
-			gen.writeEnd();  // end of array
+			gen.writeEnd(); // end of array
 			gen.close();
 			return sw.toString();
-		}
-		catch(Exception ex) {
+		} catch (Exception ex) {
 			System.out.println(ex.getMessage());
 		}
-		return "[]" ; // empty array
-		
+		return "[]"; // empty array
+
 	}
-	
-	
+
 	@PUT
 	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	public void updateSalary(@FormParam("id") int id, @FormParam("salary") int salary) {
-		// code 
+		System.out.println("updateSalary");
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			try (Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "hr", "hr")) {
+
+				PreparedStatement ps = con.prepareStatement
+						("update employees set salary = ? where employee_id = ?");
+				ps.setInt(1, salary);
+				ps.setInt(2, id);
+
+				int count = ps.executeUpdate();
+
+				if (count != 1)
+					throw new NotFoundException();
+			}
+
+		} catch (SQLException  | ClassNotFoundException ex) {
+			System.out.println(ex.getMessage());
+			throw new InternalServerErrorException();
+		}
+	}
+	
+	
+	
+	@PUT
+	@Path(("/{id}"))
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public void updateSalary2(@PathParam("id") int id, @FormParam("salary") int salary) {
+		System.out.println("updateSalary2");
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			try (Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "hr", "hr")) {
+
+				PreparedStatement ps = con.prepareStatement
+						("update employees set salary = ? where employee_id = ?");
+				ps.setInt(1, salary);
+				ps.setInt(2, id);
+
+				int count = ps.executeUpdate();
+
+				if (count != 1)
+					throw new NotFoundException();
+			}
+
+		} catch (SQLException  | ClassNotFoundException ex) {
+			System.out.println(ex.getMessage());
+			throw new InternalServerErrorException();
+		}
 	}
 
-
-
 	
+	@DELETE
+	@Path(("/{id}"))
+	public void deleteEmployee(@PathParam("id") int id) {
+	}
+	
+
 }
